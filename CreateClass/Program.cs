2fc6b852567de12a48f2b08f77fd52f6c25f9101
ngAtesz <attila.molnar@codecool.com>
2017-06-20 22:40:16 +0200

    Person class created.

--- CreateClass/Program.cs
+++ CreateClass/Program.cs
@@ -6,7 +6,32 @@ namespace CreateClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var atesz = new Person("Atesz", new DateTime(1989,5,6));
+            Console.WriteLine(atesz);
+
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+
+        public Person(string name, DateTime birthDate)
+        {
+            Name = name;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return $"My name is {Name} and I was born in {BirthDate.Year}.";
         }
     }
 }
